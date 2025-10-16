f5157ac79f26c135a4bc3ef88bef89919bd546a0
codebetterthanyesterday <fathinabrar.a@gmail.com>
2025-10-16 14:31:46 +0700

    Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'email' => 'test@user.test',
+            'name' => 'Test User',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password123'),
+        ]);
     }
 }
